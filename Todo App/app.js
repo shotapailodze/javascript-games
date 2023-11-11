fc2331e037796f0f736df4a3aa3322bae59a58cc
shotapailodze <shotikophailodze1998@gmail.com>
2023-11-11 22:36:08 +0400

    todo add/remove button and function

--- Todo App/app.js
+++ Todo App/app.js
@@ -1,3 +1,34 @@
-{
-	test
+// Selectors
+const todoInput = document.querySelector('.todo-input')
+const todoButton = document.querySelector('.todo-button')
+const todoList = document.querySelector('.todo-list')
+
+// Event Listeners
+
+todoButton.addEventListener('click', addTodo)
+
+//Functions
+
+function addTodo(event) {
+	event.preventDefault()
+	//Todo Div
+	const todoDiv = document.createElement('div')
+	todoDiv.classList.add('todo')
+	//Create LI
+	const newTodo = document.createElement('li')
+	newTodo.innerText = 'Hey'
+	newTodo.classList.add('todo-item')
+	todoDiv.appendChild(newTodo)
+	//Check Button
+	const completedButton = document.createElement('button')
+	completedButton.innerHTML = '<i class="fa-solid fa-check"></i>'
+	completedButton.classList.add('complete-button')
+	todoDiv.appendChild(completedButton)
+	//Delete Button
+	const trashButton = document.createElement('button')
+	trashButton.innerHTML = '<i class="fa-solid fa-trash"></i>'
+	trashButton.classList.add('trash-button')
+	todoDiv.appendChild(trashButton)
+	//Append to list
+	todoList.appendChild(todoDiv)
 }
