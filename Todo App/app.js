b29c62ae7870f6c5ba77f68fe81924e32e341572
shotapailodze <shotikophailodze1998@gmail.com>
2023-11-12 00:25:18 +0400

    added select of completed and not completed

--- Todo App/app.js
+++ Todo App/app.js
@@ -2,86 +2,40 @@
 const todoInput = document.querySelector('.todo-input')
 const todoButton = document.querySelector('.todo-button')
 const todoList = document.querySelector('.todo-list')
+const filterOption = document.querySelector('.filter-todo')
 
 // Event Listeners
 
 todoButton.addEventListener('click', addTodo)
 todoList.addEventListener('click', deleteCheck)
-
-//Custom Alert
-function CustomAlert() {
-	this.alert = function (message, title) {
-		document.body.innerHTML =
-			document.body.innerHTML +
-			'<div id="dialogoverlay"></div><div id="dialogbox" class="slit-in-vertical"><div><div id="dialogboxhead"></div><div id="dialogboxbody"></div><div id="dialogboxfoot"></div></div></div>'
-
-		let dialogoverlay = document.getElementById('dialogoverlay')
-		let dialogbox = document.getElementById('dialogbox')
-
-		let winH = window.innerHeight
-		dialogoverlay.style.height = winH + 'px'
-
-		dialogbox.style.top = '100px'
-
-		dialogoverlay.style.display = 'block'
-		dialogbox.style.display = 'block'
-
-		document.getElementById('dialogboxhead').style.display = 'block'
-
-		if (typeof title === 'undefined') {
-			document.getElementById('dialogboxhead').style.display = 'none'
-		} else {
-			document.getElementById('dialogboxhead').innerHTML =
-				'<i class="fa fa-exclamation-circle" aria-hidden="true"></i> ' +
-				title
-		}
-		document.getElementById('dialogboxbody').innerHTML = message
-		document.getElementById('dialogboxfoot').innerHTML =
-			'<button class="pure-material-button-contained active" onclick="customAlert.ok()">OK</button>'
-	}
-
-	this.ok = function () {
-		document.getElementById('dialogbox').style.display = 'none'
-		document.getElementById('dialogoverlay').style.display = 'none'
-	}
-}
-
-let customAlert = new CustomAlert()
+filterOption.addEventListener('click', filterTodo)
 
 //Functions
 
 function addTodo(event) {
 	event.preventDefault()
-	// If Empty Dont Do Anything
-	if (todoInput.value !== '') {
-		//Todo Div
-		const todoDiv = document.createElement('div')
-		todoDiv.classList.add('todo')
-		//Create LI
-		const newTodo = document.createElement('li')
-		newTodo.innerText = todoInput.value
-		newTodo.classList.add('todo-item')
-		todoDiv.appendChild(newTodo)
-		//Check Button
-		const completedButton = document.createElement('button')
-		completedButton.innerHTML = '<i class="fa-solid fa-check"></i>'
-		completedButton.classList.add('complete-button')
-		todoDiv.appendChild(completedButton)
-		//Delete Button
-		const trashButton = document.createElement('button')
-		trashButton.innerHTML = '<i class="fa-solid fa-trash"></i>'
-		trashButton.classList.add('trash-button')
-		todoDiv.appendChild(trashButton)
-		//Append to list
-		todoList.appendChild(todoDiv)
-		// Empty out the input
-		todoInput.value = ''
-	} else {
-		todoButton.addEventListener(
-			'click',
-			customAlert.alert('Enter Your Todo', 'Heads UP!')
-		)
-	}
+	//Todo Div
+	const todoDiv = document.createElement('div')
+	todoDiv.classList.add('todo')
+	//Create LI
+	const newTodo = document.createElement('li')
+	newTodo.innerText = todoInput.value
+	newTodo.classList.add('todo-item')
+	todoDiv.appendChild(newTodo)
+	//Check Button
+	const completedButton = document.createElement('button')
+	completedButton.innerHTML = '<i class="fa-solid fa-check"></i>'
+	completedButton.classList.add('complete-button')
+	todoDiv.appendChild(completedButton)
+	//Delete Button
+	const trashButton = document.createElement('button')
+	trashButton.innerHTML = '<i class="fa-solid fa-trash"></i>'
+	trashButton.classList.add('trash-button')
+	todoDiv.appendChild(trashButton)
+	//Append to list
+	todoList.appendChild(todoDiv)
+	// Empty out the input
+	todoInput.value = ''
 }
 
 function deleteCheck(e) {
@@ -101,4 +55,28 @@ function deleteCheck(e) {
 		const todo = item.parentElement
 		todo.classList.toggle('completed')
 	}
+}
+
+function filterTodo(e) {
+	const todos = todoList.childNodes
+	todos.forEach(function (todo) {
+		switch (e.target.value) {
+			case 'all':
+				todo.style.display = 'flex'
+				break
+			case 'completed':
+				if (todo.classList.contains('completed')) {
+					todo.style.display = 'flex'
+				} else {
+					todo.style.display = 'none'
+				}
+				break
+			case 'uncompleted':
+				if (!todo.classList.contains('completed')) {
+					todo.style.display = 'flex'
+				} else {
+					todo.style.display = 'none'
+				}
+		}
+	})
 }
